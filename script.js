0ba2584601d778103d099c8b1397b17380b76b80
nesmanpro <nesmanpro@gmail.com>
2023-07-23 19:45:27 +0200

    Pre-entrega 2 finalizada

--- script.js
+++ script.js
@@ -1,100 +1,77 @@
 // Variables necesarias
 
 let carrito = [];
-
-// lista de productos 
-
 const productos = [{
-    Id: 1,
-    nombre: "Camiseta",
-    precio: 29.5
-},
-{
-    Id: 2,
-    nombre: "Gorra",
-    precio: 40.9
-},
-{
-    Id: 3,
-    nombre: "Jersey",
-    precio: 60
-},
-{
-    Id: 4,
-    nombre: "Sombrero",
-    precio: 75.5
-},
-{
-    Id: 5,
-    nombre: "Bicicleta",
-    precio: 1100
-},
-{
-    Id: 6,
-    nombre: "Mochila",
-    precio: 120
-}
-];
-
-
-console.log('Estos son los productos que hay ahora mismo disponibles:');
-let updatedList = productos.map((producto) => {
-    console.table(`Productos añadidos: (ID: ${producto.Id}): ${producto.nombre},\nprecio: $${producto.precio}`);
-});
-
-// prompt para iniciar el ciclo o no
-let newProd = prompt('Viendo la lista de productos en la consola, quieres añadir algo al carrito? Escribe S para SI, o N para NO').toLocaleUpperCase();
-
-
-function TotalCarrito(){
-    let total = 0;
-    carrito.forEach((producto) => {
-        total += producto.precio;
-    })
-    return total;
-}
-
-
-
-
-// console.table("Carrito:", carrito);
-
-
-
-
-
-while(newProd != 'N'){
-    if(newProd != 'S'){
-        alert('Vaya! Algo ha slido mal. 🚨 \n Ingresa S para SI, N para NO.');
-    }else{
-        function AgregarAlCarrito(idProducto) {
-            const prodEncontrado = productos.find((producto) =>producto.Id === idProducto);
-            if (prodEncontrado){
-                carrito.push(prodEncontrado);
-                console.log(`Producto "${prodEncontrado.nombre}" agregado al carrito.`);
-            } else {
-                console.log('Producto no encontrado. 🤔');
-            }
-        }
-        AgregarAlCarrito(parseInt(prompt('Indica el ID del producto que quieres añadir')))
+        id: 1,
+        nombre: "Camiseta",
+        precio: 29.99
+    },
+    {
+        id: 2,
+        nombre: "Gorra",
+        precio: 40.5
+    },
+    {
+        id: 3,
+        nombre: "Jersey",
+        precio: 60.5
+    },
+    {
+        id: 4,
+        nombre: "Sombrero",
+        precio: 75.5
+    },
+    {
+        id: 5,
+        nombre: "Bicicleta",
+        precio: 1100
+    },
+    {
+        id: 6,
+        nombre: "Mochila",
+        precio: 120
     }
-    newProd = prompt('Quieres añadir algo más al carrito? Escribe S para SI, o N para NO').toLocaleUpperCase();
-};
-
-// console.log("Total del carrito: " + (TotalCarrito() * 1.21) + '€');
-
-// Enumera los productos que hay en el carrito y su precio
+];
+console.log('Esta es la lista de productos disponibles:');
+console.table(productos);
 
 
-
-// Funcion para calcular el precio subtotal + iva
-function totalCalc(carrito){
-    let total = carrito.reduce((acum, product) => acum + product.precio, 0)
-    return total;
+// Función para agregar un producto al carrito
+function agregarAlCarrito(idProducto) {
+    const productoEncontrado = productos.find((producto) => producto.id === idProducto);
+    if (productoEncontrado) {
+        carrito.push(productoEncontrado);
+        console.log(`Producto "${productoEncontrado.nombre}" (${productoEncontrado.precio}€) agregado al carrito.`);
+    } else {
+        alert("Producto no encontrado.");
+    }
 }
 
 
 // Función para calcular el total del carrito
+function calcularTotalCarrito() {
+    let total = 0;
+    carrito.forEach((producto) => {
+        total += producto.precio;
+    });
+    return total;
+}
 
-const totalprecio = totalCalc(carrito)
-console.log(`Lo que da un total de: ${totalCalc() * 1.21}  iva incluido`);
+
+// Función para que el usuario agregue productos al carrito
+function agregarProductosAlCarrito() {
+    let idProducto = parseInt(prompt("Observa la lista de productos en la consola. \nIngrese un ID para agregar un producto al carrito (0 para salir):"));
+
+    while (idProducto !== 0) {
+        agregarAlCarrito(idProducto);
+        idProducto = parseInt(prompt("Desea agregar algun otro producto al carrito (0 para salir):"));
+    }
+
+    console.log('Estos son los productos de tu carrito:');
+    console.table(carrito);
+    console.log('Total a pagar: ' + calcularTotalCarrito() +'€');
+
+}
+
+// Llamando a la función agregarProductosAlCarrito
+agregarProductosAlCarrito();
